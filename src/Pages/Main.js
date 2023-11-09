4384b1282b56c3d64ec02b20536fd52e8b62e27e
barinale <barinale08@gmail.com>
2023-11-09 11:17:24 +0100

    finihsh part for socile meida now i should work in image and logo then go to meadia screen sizes

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import './Main.css'
 import { Header } from '../Components/header/Header'
 import { Info } from '../Components/header/Info'
+import { SocialMedia } from '../Components/header/SocialMedia'
 export const Main = () => {
   
  
 
   return (
     <div id="MainSection">
-     <Header />
-    <Info />
+        <Header />
+        <Info />
+        <SocialMedia />
     </div>
   )
 }
